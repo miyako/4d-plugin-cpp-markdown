f225d4c441bcbdafdfa8a21e4030e51d4af12b7f
Keisuke Miyako <keisuke.miyako@4d.com>
2019-12-13 13:40:36 +0900

    2.0.1

--- CPP Markdown/build/CPP Markdown.build/Deployment/CPP Markdown.build/Objects-normal/x86_64/4DPluginAPI.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/miyako/Documents/miyako@github.com/4d-plugin-cpp-markdown/CPP\ Markdown/4D\ Plugin\ API/4DPluginAPI.c \
-  /Users/miyako/Documents/miyako@github.com/4d-plugin-cpp-markdown/CPP\ Markdown/4D\ Plugin\ API/4DPluginAPI.h \
-  /Users/miyako/Documents/miyako@github.com/4d-plugin-cpp-markdown/CPP\ Markdown/4D\ Plugin\ API/Flags.h \
-  /Users/miyako/Documents/miyako@github.com/4d-plugin-cpp-markdown/CPP\ Markdown/4D\ Plugin\ API/PublicTypes.h \
-  /Users/miyako/Documents/miyako@github.com/4d-plugin-cpp-markdown/CPP\ Markdown/4D\ Plugin\ API/PrivateTypes.h \
-  /Users/miyako/Documents/miyako@github.com/4d-plugin-cpp-markdown/CPP\ Markdown/4D\ Plugin\ API/EntryPoints.h
--- CPP Markdown/build/CPP Markdown.build/Deployment/CPP Markdown.build/Objects-normal/x86_64/markdown-tokens.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/miyako/Documents/miyako@github.com/4d-plugin-cpp-markdown/CPP\ Markdown/include/markdown-tokens.cpp \
-  include/markdown-tokens.h include/markdown.h
